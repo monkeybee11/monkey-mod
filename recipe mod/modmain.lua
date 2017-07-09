7c6f957085015f003e1c21885cce420dcf8f9bd0
monkeybee11 <monkeybee1@googlemail.com>
2017-07-09 16:39:50 +0100

    Add files via upload
    some how buged the item by givingit a crafting recipe

--- recipe mod/modmain.lua
+++ recipe mod/modmain.lua
@@ -1,13 +1,15 @@
+--local require = GLOBAL.require
 local STRINGS = GLOBAL.STRINGS
 
---[[
 
-This mod shows how to load custom prefabs and assets so that they are usable
-by the game.
-
---]]
 GLOBAL.STRINGS.SOOTY = "sooty"
 GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.SOOTY = "monkeys favrout teddy bear"
+
+local sooty = AddRecipe("sooty", {Ingredient("beefalowool", 10),Ingredient("reviver", 1),Ingredient("redgem", 2)}, GLOBAL.RECIPETABS.MAGIC, GLOBAL.TECH.NONE, nil, nil, nil, nil, nil, "images/inventoryimages/sooty.xml", "sooty.tex")
+
+STRINGS.NAMES.SOOTY = "sooty"
+STRINGS.RECIPE_DESC.SOOTY = "sooty will keep us safe at night"
+
 
 -- The PrefabFiles list is the list of all the files in your <modname>/scripts/prefabs folder
 -- that you want to load prefabs from
