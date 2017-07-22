70ee423f2f5cef221d778440853aee624fd767a0
monkeybee11 <monkeybee1@googlemail.com>
2017-07-22 12:29:15 +0100

    Add files via upload

--- recipe mod/modmain.lua
+++ recipe mod/modmain.lua
@@ -1,5 +1,6 @@
 PrefabFiles = {
     "sooty",
+    "birdy",
 }
 
 	
@@ -7,6 +8,7 @@ PrefabFiles = {
 {
 	
     Asset("ATLAS", "images/inventoryimages/sooty.xml"),
+    Asset("ATLAS", "images/inventoryimages/birdy.xml"),
 
 }
  
@@ -27,14 +29,20 @@ PrefabFiles = {
         GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.SOOTY = "monkeys favrout teddy"
 
 
-
-
+        -- birdy info
+        GLOBAL.STRINGS.NAMES.BIRDY = "birdy"
+        STRINGS.RECIPE_DESC.BIRDY = "birdy!!! tweet tweet tweet"
+        GLOBAL.STRINGS.CHARACTERS.GENERIC.DESCRIBE.BIRDY = "birdy!!!! tweet tweet tweet birdy!!!"
 
 
 
         -- sooty Recipe
         local sooty = GLOBAL.Recipe("sooty",{ Ingredient("beefalowool", 10), Ingredient("redgem", 2), Ingredient("reviver", 1) },GLOBAL.RECIPETABS.MAGIC, GLOBAL.TECH.NONE, nil, nil, nil, nil, nil, "images/inventoryimages/sooty.xml", "sooty.tex" )
         sooty.atlas = "images/inventoryimages/sooty.xml"
+
+        -- birdy recipe
+        local birdy = GLOBAL.Recipe("birdy",{ Ingredient("feather_robin", 10), Ingredient("bluegem", 2), Ingredient("reviver", 1) },GLOBAL.RECIPETABS.MAGIC, GLOBAL.TECH.NONE, nil, nil, nil, nil, nil, "images/inventoryimages/birdy.xml", "birdy.tex" )
+        birdy.atlas = "images/inventoryimages/birdy.xml"
 
 
 
---
+++ recipe mod/scripts/prefabs/birdy.lua
@@ -0,0 +1,73 @@
+local assets=
+{
+     Asset("ANIM", "anim/birdy.zip"),
+	 
+	 Asset("ATLAS", "images/inventoryimages/birdy.xml"),
+	 Asset("IMAGE", "images/inventoryimages/birdy.tex"),
+}
+prefabs = {
+     "birdy",
+}
+local function onfinished(inst)
+    inst:Remove()
+end
+
+local function topocket(inst, owner)
+	local attractor = owner.components.birdattractor
+        if attractor then
+            attractor.spawnmodifier:RemoveModifier(inst)
+
+            local birdspawner = TheWorld.components.birdspawner
+            if birdspawner ~= nil then
+                birdspawner:ToggleUpdate(true)
+            end
+        end
+end
+
+local function toground(inst)
+        local attractor = owner.components.birdattractor
+        if attractor then
+            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_MAXDELTA_FEATHERHAT, "maxbirds")
+            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_DELAYDELTA_FEATHERHAT.MIN, "mindelay")
+            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_DELAYDELTA_FEATHERHAT.MAX, "maxdelay")
+            
+            local birdspawner = TheWorld.components.birdspawner
+            if birdspawner ~= nil then
+                birdspawner:ToggleUpdate(true)
+            end
+        end
+end
+
+local function fn(sim)
+    local inst = CreateEntity()
+	local trans = inst.entity:AddTransform()
+	local anim = inst.entity:AddAnimState()
+	local sound = inst.entity:AddSoundEmitter()
+	
+	inst.entity:AddTransform()
+	inst.entity:AddAnimState()
+	inst.entity:AddNetwork()
+	inst:ListenForEvent("onputininventory", topocket)
+    inst:ListenForEvent("ondropped", toground)
+    toground(inst)
+	
+	MakeInventoryPhysics(inst)
+	
+	anim:SetBlank("birdy")
+	anim:SetBuild("birdy")
+	anim:PlayAnimation("idle")
+	
+	           if not TheWorld.ismastersim then
+		    return inst
+		   end
+		    inst.entity:SetPristine()
+			
+	inst:AddComponent("inspectable")
+	
+	inst:AddComponent("inventoryitem")
+	inst.components.inventoryitem.imagename = "birdy"
+	inst.components.inventoryitem.atlasname = "images/inventoryimages/birdy.xml"
+	
+	return inst
+end
+return Prefab("common/inventory/birdy", fn, assets, prefabs)
