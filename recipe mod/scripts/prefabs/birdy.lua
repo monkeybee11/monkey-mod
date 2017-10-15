26faceb3114699535a4a9af3321561af1473793b
monkeybee11 <monkeybee1@googlemail.com>
2017-10-15 17:39:01 +0100

    Add files via upload

--- recipe mod/scripts/prefabs/birdy.lua
+++ recipe mod/scripts/prefabs/birdy.lua
@@ -7,36 +7,36 @@ local assets=
 }
 prefabs = {
      "birdy",
-}
-local function onfinished(inst)
-    inst:Remove()
-end
+} 
+local function onfinished(inst) TheNet:Say("1",false)
+    inst:Remove() TheNet:Say("2",false)
+end TheNet:Say("3",false)
 
-local function toground(inst)
-    local attractor = TheWorld.components.birdattractor
-        if attractor then
-            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_MAXDELTA_BIRDY, "maxbirds")
-            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_DELAYDELTA_BIRDY.MIN, "mindelay")
-            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_DELAYDELTA_BIRDY.MAX, "maxdelay")
+local function toground(inst) TheNet:Say("4",false) --work
+    local attractor = TheWorld.components.birdattractor TheNet:Say("5",false) --work
+        if attractor then TheNet:Say("6",false)
+            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_MAXDELTA_BIRDY, "maxbirds") TheNet:Say("7",false)
+            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_DELAYDELTA_BIRDY.MIN, "mindelay") TheNet:Say("8",false)
+            attractor.spawnmodifier:SetModifier(inst, TUNING.BIRD_SPAWN_DELAYDELTA_BIRDY.MAX, "maxdelay") TheNet:Say("9",false)
             
-            local birdspawner = TheWorld.components.birdspawner
-            if birdspawner ~= nil then
-                birdspawner:ToggleUpdate(true)
-            end
-    end
-end
+            local birdspawner = TheWorld.components.birdspawner TheNet:Say("10",false)
+            if birdspawner ~= nil then TheNet:Say("11",false)
+                birdspawner:ToggleUpdate(true) TheNet:Say("12",false)
+            end TheNet:Say("13",false)
+    end TheNet:Say("14",false) --work
+end TheNet:Say("15",false)
 
-local function topocket(inst)
-	local attractor = TheWorld.components.birdattractor
-        if attractor then
-            attractor.spawnmodifier:RemoveModifier(inst)
+local function topocket(inst) TheNet:Say("16",false) -- work
+	local attractor = TheWorld.components.birdattractor TheNet:Say("17",false) -- work
+        if attractor then TheNet:Say("18",false)
+            attractor.spawnmodifier:RemoveModifier(inst) TheNet:Say("19",false)
 
-            local birdspawner = TheWorld.components.birdspawner
-            if birdspawner ~= nil then
-                birdspawner:ToggleUpdate(true)
-            end
-    end
-end
+            local birdspawner = TheWorld.components.birdspawner TheNet:Say("20",false)
+            if birdspawner ~= nil then TheNet:Say("21",false)
+                birdspawner:ToggleUpdate(true) TheNet:Say("22",false)
+            end TheNet:Say("24",false)
+    end TheNet:Say("25",false)  --work
+end TheNet:Say("26",false)
 
 local function fn(sim)
     local inst = CreateEntity()
