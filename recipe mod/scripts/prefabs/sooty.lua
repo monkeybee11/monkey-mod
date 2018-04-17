05ed46d678f99789493f30ede52833a516527f24
monkeybee11 <monkeybee1@googlemail.com>
2018-04-17 18:28:11 +0100

    Add files via upload

--- recipe mod/scripts/prefabs/sooty.lua
+++ recipe mod/scripts/prefabs/sooty.lua
@@ -20,32 +20,38 @@ end TheNet:Say("4")
 local function onclose(inst)
 end  TheNet:Say("5")
 
-	local function LightBattery(item)
-		return item:HasTag("lightbattery")
-	end  TheNet:Say("6")
-	
-	local function CheckBattery(inst)
-		local lightbattery = --[[ works for all forms ]] FindBattery(inst, LightBattery)
-		
-		if inst._lightbattery ~= lightbattery then TheNet:Say("check battery 1")
-			if lightbattery ~= nil then
-				if inst._lightbattery == nil then
-					for i, fx_prefab in ipairs(inst:GetSkinName() == nil and { "torchfire" } or SKIN_FX_PREFAB[inst:GetSkinName()] or {}) do
-					local fx = SpawnPrefab(fx_prefab)
-					fx.entity:SetParent(owner.entity)
-					fx.entity:AddFollower()
-					fx.Follower:FollowSymbol(owner.GUID, "swap_object", 0, fx.fx_offset, 0)
+local function LightBattery(item)
+	return item:HasTag("lightbattery")
+end  TheNet:Say("6")
 
-					table.insert(inst.fires, fx)
-					inst.Light:Enable(true)
-					end
-				end
-			elseif inst._lightbattery ~= nil then
-				inst.Light:Enable(false)
+local function FindBattery(inst, fn)
+    return inst.components.container:FindItem(fn)
+end
+
+local function lightbattery(inst)
+end
+
+local function OnEquip(inst, owner)
+    owner.AnimState:OverrideSymbol("swap_object", "swap_sooty", "sooty") -- this is kinda a folder parth swap_object = exported folder > swap_prefab > prefab
+    owner.AnimState:Show("ARM_carry")
+    owner.AnimState:Hide("ARM_normal")
+	if inst._lightbattery ~= lightbattery then TheNet:Say("check battery 1") -- its only checking for this on world load up ...y
+	if lightbattery ~= nil then
+		if inst._lightbattery == nil then TheNet:Say("ive got a battery") -- its only checking for this on world load up ...y
+			inst.Light:Enable(true)
 			end
-			inst._lightbattery = lightbattery
 		end
-	end  TheNet:Say("7")
+	elseif inst._lightbattery ~= nil then
+		inst.Light:Enable(false)
+end
+	inst._lightbattery = lightbattery
+	TheNet:Say("OnEquip work")
+end   -- this dose a thing im not sure what think it alowes to to go in to the hand slot
+ 
+local function OnUnequip(inst, owner)
+    owner.AnimState:Hide("ARM_carry")
+    owner.AnimState:Show("ARM_normal")
+end   -- this dose a thing im not sure what think it alowes it to leave the hand slot
 
 
 local function fn(sim)
@@ -53,27 +59,12 @@ local function fn(sim)
     local trans = inst.entity:AddTransform()
     local anim = inst.entity:AddAnimState()
     local sound = inst.entity:AddSoundEmitter()
-		
-    local function OnEquip(inst, owner)
-        owner.AnimState:OverrideSymbol("swap_object", "swap_sooty", "sooty") -- this is kinda a folder parth swap_object = exported folder > swap_prefab > prefab
-        owner.AnimState:Show("ARM_carry")
-        owner.AnimState:Hide("ARM_normal")
-		TheNet:Say("OnEquip work")
-    end   -- this dose a thing im not sure what think it alowes to to go in to the hand slot
- 
-    local function OnUnequip(inst, owner)
-        owner.AnimState:Hide("ARM_carry")
-        owner.AnimState:Show("ARM_normal")
-    end   -- this dose a thing im not sure what think it alowes it to leave the hand slot
-	
-	local function OnLoadPostPass(inst)
-		inst:ListenForEvent("itemget", CheckBattery)
-		inst:ListenForEvent("itemlose", CheckBattery)
-		if POPULATING then
-			CheckBattery(inst)
-		end
-	end
- 
+	inst.entity:AddLight()
+	inst.Light:SetRadius(2.5)
+	inst.Light:SetIntensity(.8)
+	inst.Light:SetFalloff(.4)
+    inst.Light:SetColour(180 / 255, 195 / 255, 150 / 255)
+	inst.Light:Enable(true)
 
     inst.entity:AddTransform()    -- this dose a thing im not sure what
     inst.entity:AddAnimState()  -- this dose a thing im not sure what
@@ -85,10 +76,10 @@ local function fn(sim)
     anim:SetBuild("sooty")  -- this dose a thing im not sure what
     anim:PlayAnimation("idle")  -- this dose a thing im not sure what
 
-                if not TheWorld.ismastersim then
-            return inst
-           end  -- this dose a thing im not sure what
-            inst.entity:SetPristine()  -- this dose a thing im not sure what
+    if not TheWorld.ismastersim then
+        return inst
+    end  -- this dose a thing im not sure what
+    inst.entity:SetPristine()  -- this dose a thing im not sure what
  
     inst:AddComponent("inspectable")  -- this dose a thing im not sure what think it alowes a player to inspect the item
      
