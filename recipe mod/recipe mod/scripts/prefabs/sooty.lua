b48032ed8461fdad38576b85348518bdb0fd209a
monkeybee11 <monkeybee1@googlemail.com>
2018-04-18 11:07:11 +0100

    Add files via upload

--- recipe mod/recipe mod/scripts/prefabs/sooty.lua
+++ recipe mod/recipe mod/scripts/prefabs/sooty.lua
@@ -28,6 +28,29 @@ local function FindBattery(inst, fn)
     return inst.components.container:FindItem(fn)
 end
 
+local function lightbattery(inst)
+end
+local function OnEquip(inst, owner)
+	local lightbattery = --[[ works for all forms ]] FindBattery(inst, LightBattery)
+	if true then TheNet:Say("check battery 1") -- its only checking for this on world load up ...y
+		if inst._lightbattery == nil then TheNet:Say("ive got a battery") -- its only checking for this on world load up ...y
+			inst.Light:Enable(true)
+			end
+		elseif inst._lightbattery ~= nil then
+		inst.Light:Enable(false)
+	end
+	inst._lightbattery = lightbattery
+owner.AnimState:OverrideSymbol("swap_object", "swap_sooty", "sooty") -- this is kinda a folder parth swap_object = exported folder > swap_prefab > prefab
+owner.AnimState:Show("ARM_carry")
+owner.AnimState:Hide("ARM_normal")
+TheNet:Say("OnEquip work")
+end   -- this dose a thing im not sure what think it alowes to to go in to the hand slot
+ 
+local function OnUnequip(inst, owner)
+    owner.AnimState:Hide("ARM_carry")
+    owner.AnimState:Show("ARM_normal")
+end   -- this dose a thing im not sure what think it alowes it to leave the hand slot
+
 local function fn(sim)
     local inst = CreateEntity()
     local trans = inst.entity:AddTransform()
@@ -39,32 +62,7 @@ local function fn(sim)
 	inst.Light:SetFalloff(.4)
     inst.Light:SetColour(180 / 255, 195 / 255, 150 / 255)
 	inst.Light:Enable(true)
-		
-    local function OnEquip(inst, owner)
-        owner.AnimState:OverrideSymbol("swap_object", "swap_sooty", "sooty") -- this is kinda a folder parth swap_object = exported folder > swap_prefab > prefab
-        owner.AnimState:Show("ARM_carry")
-        owner.AnimState:Hide("ARM_normal")
-		local lightbattery = --[[ works for all forms ]] FindBattery(inst, LightBattery)
-		
-		if inst._lightbattery ~= lightbattery then TheNet:Say("check battery 1") -- its only checking for this on world load up ...y
-			if lightbattery ~= nil then
-				if inst._lightbattery == nil then TheNet:Say("ive got a battery") -- its only checking for this on world load up ...y
-					inst.Light:Enable(true)
-					end
-				end
-			elseif inst._lightbattery ~= nil then
-				inst.Light:Enable(false)
-			end
-			inst._lightbattery = lightbattery
-		TheNet:Say("OnEquip work")
-    end   -- this dose a thing im not sure what think it alowes to to go in to the hand slot
- 
-    local function OnUnequip(inst, owner)
-        owner.AnimState:Hide("ARM_carry")
-        owner.AnimState:Show("ARM_normal")
-		inst.Light:Enable(false)
-    end   -- this dose a thing im not sure what think it alowes it to leave the hand slot
-	
+
     inst.entity:AddTransform()    -- this dose a thing im not sure what
     inst.entity:AddAnimState()  -- this dose a thing im not sure what
     inst.entity:AddNetwork()  -- this dose a thing im not sure what
@@ -75,10 +73,10 @@ local function fn(sim)
     anim:SetBuild("sooty")  -- this dose a thing im not sure what
     anim:PlayAnimation("idle")  -- this dose a thing im not sure what
 
-                if not TheWorld.ismastersim then
-            return inst
-           end  -- this dose a thing im not sure what
-            inst.entity:SetPristine()  -- this dose a thing im not sure what
+    if not TheWorld.ismastersim then
+        return inst
+    end  -- this dose a thing im not sure what
+    inst.entity:SetPristine()  -- this dose a thing im not sure what
  
     inst:AddComponent("inspectable")  -- this dose a thing im not sure what think it alowes a player to inspect the item
      
@@ -91,12 +89,11 @@ local function fn(sim)
     inst.components.equippable:SetOnUnequip(OnUnequip)
 	
     inst:AddComponent("container")
-    inst.components.container:WidgetSetup("hutch")
+    inst.components.container:WidgetSetup("treasurechest")
     inst.components.container.onopenfn = onopen
     inst.components.container.onclosefn = onclose
 
 	inst.components.equippable.dapperness = TUNING.DAPPERNESS_HUGE -- this makes it oso dapper
- 
-    return inst
+     return inst
 end TheNet:Say("8")  -- this is the end
 return  Prefab("common/inventory/sooty", fn, assets, prefabs)   -- this dose a thing im not sure what
